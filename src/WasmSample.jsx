18f42ceedf3939b975d5fa8a2fc755f595481444
nguyenyou <tunt081295@gmail.com>
2025-06-29 14:14:07 +0100

    🚢 ship it

--- src/WasmSample.jsx
+++ src/WasmSample.jsx
@@ -1,71 +1,72 @@
-import wasmUrl from './pdfium.wasm?url'
-import samplePdfUrl from './sample.pdf?url'
-import createPdfium from './pdfium'
-import { functions } from './functions'
+import wasmUrl from "./pdfium.wasm?url";
+import samplePdfUrl from "./sample.pdf?url";
+import createPdfium from "./pdfium";
+import { functions } from "./functions";
 
-function malloc(size, pdfium) {
-  const ptr = pdfium.wasmExports.malloc(size);
+let mod = {}
+
+function malloc(size) {
+  const ptr = mod.pdfium.wasmExports.malloc(size);
   for (let i = 0; i < size; i++) {
-    pdfium.HEAP8[ptr + i] = 0;
+    mod.pdfium.HEAP8[ptr + i] = 0;
   }
 
   return ptr;
 }
 
-function free(ptr, pdfium) {
-  pdfium.wasmExports.free(ptr);
+function free(ptr) {
+  mod.pdfium.wasmExports.free(ptr);
 }
 
 export default function WasmSample() {
-  
   const handleInitWasm = async () => {
-    const wasmBinary = await fetch(wasmUrl).then(res => res.arrayBuffer())
-    const pdfium = await createPdfium({ wasmBinary })
-    const mod = {
-     pdfium 
-    }
+    const wasmBinary = await fetch(wasmUrl).then((res) => res.arrayBuffer());
+    const pdfium = await createPdfium({ wasmBinary });
+    mod.pdfium = pdfium;
     for (const key in functions) {
       const ident = key;
       const args = functions[ident][0];
       const ret = functions[ident][1];
       mod[ident] = pdfium.cwrap(key, ret, args);
     }
-    console.log(pdfium)
+    console.log(pdfium);
 
     mod.PDFium_Init();
 
-    const fileContent = await fetch(samplePdfUrl).then((res) => res.arrayBuffer());
+    console.log("Init DONE")
+  };
+
+  const handleLoadDocument = async () => {
+    const fileContent = await fetch(samplePdfUrl).then((res) =>
+      res.arrayBuffer()
+    );
 
     const array = new Uint8Array(fileContent);
     const length = array.length;
-    const filePtr = malloc(length, pdfium);
-    pdfium.HEAPU8.set(array, filePtr);
+    const filePtr = malloc(length);
+    mod.pdfium.HEAPU8.set(array, filePtr);
 
-    const password = ""
+    const password = "";
     const passwordBytesSize = new TextEncoder().encode(password).length + 1;
-    const passwordPtr = malloc(passwordBytesSize, pdfium);
-    pdfium.stringToUTF8(
-      password,
-      passwordPtr,
-      passwordBytesSize,
-    );
+    const passwordPtr = malloc(passwordBytesSize);
+    mod.pdfium.stringToUTF8(password, passwordPtr, passwordBytesSize);
 
-    const docPtr = mod.FPDF_LoadMemDocument(
-      filePtr,
-      length,
-      passwordPtr,
-    );
+    const docPtr = mod.FPDF_LoadMemDocument(filePtr, length, passwordPtr);
 
-    free(passwordPtr, pdfium);
-
+    free(passwordPtr);
 
     const pageCount = mod.FPDF_GetPageCount(docPtr);
-    console.log("pageCount:", pageCount)
-  }
+    console.log("pageCount:", pageCount);
+  };
 
   return (
     <div>
-      <button onClick={handleInitWasm} className="btn">Init Wasm</button>
+      <button onClick={handleInitWasm} className="btn">
+        Init Wasm
+      </button>
+      <button onClick={handleLoadDocument} className="btn">
+        Load Document
+      </button>
     </div>
   );
 }
